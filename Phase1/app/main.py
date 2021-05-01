ed9f24fc3a1e0817dd5070eb5267b62792d787ad
motazalratta <motaz.r@gmail.com>
2021-05-01 10:05:56 +0000

    code refactoring

--- Phase1/app/main.py
+++ Phase1/app/main.py
@@ -1,73 +1,128 @@
-import yaml
-import os
 import glob
-import tarfile
-import sys
+import jsonschema
 import logging
+import os
 import shutil
-import time
+import sys
+import tarfile
 import threading
+import time
+import yaml
 
 from topic import Topic
 
+# convert ymal load to dict
+class DictAsMember(dict):
+    def __getattr__(self, name):
+        value = self[name]
+        if isinstance(value, dict):
+            value = DictAsMember(value)
+        if isinstance(value, list):
+            newlist =[]
+            for i in value:
+                if isinstance(i, dict):
+                    newlist.append(DictAsMember(i))
+                else:
+                    newlist.append(i)
+            return newlist
+        return value
+
 if __name__ == '__main__':
+
     logging.getLogger().setLevel(logging.INFO)
 
+    # Check the env variables
     if os.environ.get('GOOGLE_APPLICATION_CREDENTIALS') is None:
-        print('the env GOOGLE_APPLICATION_CREDENTIALS is null')
+        logging.error('The env GOOGLE_APPLICATION_CREDENTIALS is null')
         sys.exit(-1)
-
     if os.environ.get('GOOGLE_CLOUD_PROJECT') is None:
-        print('the env GOOGLE_CLOUD_PROJECT is null')
+        logging.error('The env GOOGLE_CLOUD_PROJECT is null')
         sys.exit(-1)
 
+    # Read the config schema
     try:
-        with open("config/config.yml", "r") as ymlfile:
-            config = yaml.load(ymlfile, Loader=yaml.FullLoader)
+        with open('configschema.json', 'r') as cs:
+            schemaobject = yaml.safe_load(cs)
     except IOError as e:
-        print('Cannot open the config file: {}'.format(e))
-        sys.exit(1)
+        logging.error('Cannot open the config schema file: {}'.format(e))
+        sys.exit(-1)
 
-    batch_max_messages = config['pubsub']["batch_settings"]["max_messages"]
-    batch_max_bytes    = config['pubsub']["batch_settings"]["max_bytes"]
-    batch_max_latency  = config['pubsub']["batch_settings"]["max_latency"]
-    batch_max_threads  = config['pubsub']["batch_settings"]["max_threads"]
-    
-    topics= {}
-    for topic in config['pubsub']['topics'].values():
-        topics[topic["input_filename"]] = Topic(os.environ.get('GOOGLE_CLOUD_PROJECT'),topic["topic_id"],
-                                                topic["schema_id"],topic["proto_path"],topic["input_filename"],
-                                                topic["batch_publish"], batch_max_messages, batch_max_bytes, batch_max_latency,batch_max_threads
-                                                )
+    # Read the config file
+    try:
+        with open('config{}config.yml'.format(os.path.sep)) as ymlfile:
+            configyml = yaml.load(ymlfile, Loader=yaml.FullLoader)
+    except IOError as e:
+        logging.error('Cannot open the config file: {}'.format(e))
+        sys.exit(-1)
 
-    arcivedir=config['pubsub']['archive_file_dir']
-    if not os.path.exists(arcivedir):
-        os.mkdir(arcivedir)
+    # Validate the config file
+    try:
+        jsonschema.validate(configyml, schemaobject)
+    except Exception as e:
+        logging.error(
+            'An exception occurred while validating the config file\n{}'.format(e))
+        sys.exit(-1)
 
-    
+    # Convert config to dict
+    config = DictAsMember(configyml)
+
+    # Create all topics
+    topics = {}
+    for topic in config.topics:
+        topics[topic.input_filename]  = Topic(
+            project_id=os.environ.get("GOOGLE_CLOUD_PROJECT"),
+            topic_id=topic.topic_id,
+            schema_id=topic.schema_id,
+            proto_path=topic.proto_path,
+            input_filename=topic.input_filename,
+            batch_publish=topic.batch_publish,
+
+            batch_max_messages=config.batch_settings.max_messages,
+            batch_max_bytes=config.batch_settings.max_bytes,
+            batch_max_latency=config.batch_settings.max_latency,
+            batch_max_threads=config.batch_settings.max_threads
+        )
+
+    # Check whether raw_files_dir is an existing directory or not
+    if not os.path.isdir(config.raw_files_dir):
+        logging.error('Can not find raw_files_dir: {}'.format(
+            config.raw_files_dir))
+        sys.exit(-1)
+
+    # Create the archive_files_dir if it is not exist
+    arcivedir = config.archive_files_dir
+    try:
+        os.makedirs(arcivedir, exist_ok=True)
+    except Exception as e:
+        logging.error('Can not create archive_files_dir: {}\n{}'.format(
+            config.archive_files_dir),e)
+        sys.exit(-1)
+
     while True:
-        rawfiles=glob.glob(config['pubsub']['raw_file_dir']+'/*.tar.gz')
-        logging.info(f"Scan all raw files :{rawfiles}")
+        rawfiles = glob.glob(config.raw_files_dir + os.path.sep + '*.tar.gz')
+        logging.info('Scan all raw files :{}'.format(rawfiles))
         for targzfile in rawfiles:
-            # process the file
-            with tarfile.open(targzfile, "r:gz") as tar:
+            # Read all tar.gz files
+            with tarfile.open(targzfile, 'r:gz') as tar:
                 for tarinfo in tar:
-                    line_counter=0
+                    line_counter = 0
+                    # Read the files inside tar.gz without decompressing
                     if tarinfo.isreg():
                         if tarinfo.name in topics:
-                            logging.info(f"Read {tarinfo.name}.")
-                            f = tar.extractfile(tarinfo.name)  
+                            logging.info(f'Read {tarinfo.name}.')
+                            f = tar.extractfile(tarinfo.name)
                             content = f.read().decode('utf-8', errors='ignore')
                             lines = content.split('\n')
+                            # Publish line by line
                             for i, line in enumerate(lines):
                                 if line:
-                                    logging.info(f"file={tarinfo.name};line={line_counter};threads_count={threading.active_count()}")
+                                    logging.info(
+                                        f'file={tarinfo.name};line={line_counter};threads_count={threading.active_count()}')
                                     topics[tarinfo.name].publish(line)
-                                line_counter=line_counter+1
-            # move the processed file to archive
+                                line_counter = line_counter+1
+            # Move the processed file to archive folder
             filename = os.path.basename(targzfile)
-            dest = os.path.join(arcivedir,filename)
-            shutil.move(targzfile,dest)
-        logging.info(f"Waiting the next scan ...")
+            dest = os.path.join(arcivedir, filename)
+            shutil.move(targzfile, dest)
+        logging.info(f'Waiting the next scan ...')
         time.sleep(60)
-            
--- Phase1/app/topic.py
+++ Phase1/app/topic.py
@@ -1,6 +1,7 @@
 import logging
 import time
 import threading
+import sys
 
 from google.api_core.exceptions import AlreadyExists,InvalidArgument
 from google.cloud.pubsub import SchemaServiceClient,PublisherClient
@@ -26,73 +27,76 @@ class Topic:
             max_latency=batch_max_latency,
         )
         
-
         self.open_threads=0
         self.batch_max_threads = batch_max_threads
 
+        # Construct batch_publish instance
         if self.batch_publish:
             self.publisher_client = PublisherClient(batch_settings)
         else:
             self.publisher_client = PublisherClient()
-
+        
+        self.schema_client = SchemaServiceClient()
+        self.schema_path = self.schema_client.schema_path(self.project_id, self.schema_id)
         self.topic_path = self.publisher_client.topic_path(project_id, topic_id)
 
-        # create the topic
+        # Create the schema and topic 
         self.create_proto_schema()
         self.create_topic_with_schema()
 
     def create_proto_schema(self):
-        project_path = f"projects/{self.project_id}"
+        project_path = f'projects/{self.project_id}'
 
-        # Read a protobuf schema file as a string.
-        with open(self.proto_path, "rb") as f:
-            proto_source = f.read().decode("utf-8")
+        # Read a protobuf schema file as a string
+        try:
+            with open(self.proto_path, 'rb') as f:
+                    proto_source = f.read().decode('utf-8')
+        except IOError as e:
+            logging.error('Cannot open the protobuf schema file: {}'.format(e))
+            sys.exit(1)
 
-        schema_client = SchemaServiceClient()
-        schema_path = schema_client.schema_path(self.project_id, self.schema_id)
         schema = Schema(
-            name=schema_path, type_=Schema.Type.PROTOCOL_BUFFER, definition=proto_source
+            name=self.schema_path, type_=Schema.Type.PROTOCOL_BUFFER, definition=proto_source
         )
 
         try:
-            result = schema_client.create_schema(
-                request={"parent": project_path, "schema": schema, "schema_id": self.schema_id}
+            result = self.schema_client.create_schema(
+                request={'parent': project_path, 'schema': schema, 'schema_id': self.schema_id}
             )
-            logging.info(f"Created a schema using a protobuf schema file:\n{result}")
+            logging.info(f'Created a schema using a protobuf schema file:\n{result}')
         except AlreadyExists:
-            logging.info(f"{self.schema_id} schema already exists.")
+            logging.info(f'{self.schema_id} schema already exists')
+        except Exception as e:
+            logging.error('An exception occurred while creating schema {}\n{}'.format(self.schema_id,e))
+            sys.exit(-1)
 
     def create_topic_with_schema(self):
-        topic_path = self.publisher_client.topic_path(self.project_id, self.topic_id)
-
-        schema_client = SchemaServiceClient()
-        schema_path = schema_client.schema_path(self.project_id, self.schema_id)
-
         try:
             response = self.publisher_client.create_topic(
                 request={
-                    "name": topic_path,
-                    "schema_settings": {"schema": schema_path, "encoding": Encoding.JSON},
+                    'name': self.topic_path,
+                    'schema_settings': {'schema': self.schema_path, 'encoding': Encoding.JSON},
                 }
             )
-            logging.info(f"Created a topic:\n{response}")
-
+            logging.info(f'Created a topic:\n{response}')
         except AlreadyExists:
-            logging.info(f"{self.topic_id} topic already exists.")
+            logging.info(f'{self.topic_id} topic already exists')
+        except Exception as e:
+            logging.error('An exception occurred while creating topic {}\n{}'.format(self.topic_id,e))
+            sys.exit(-1)
     
     def publish(self,json_string):
-        data = str(json_string).encode("utf-8")
+        data = str(json_string).encode('utf-8')
         if self.batch_publish:
             future = self.publisher_client.publish(self.topic_path, data)
             future.add_done_callback(self.callback)
+            # Limit the open threads
             while threading.active_count()>self.batch_max_threads:
-                logging.info(f"The open threads {threading.active_count()} > {self.batch_max_threads} => waiting 2 seconds")
+                logging.info(f'The open threads {threading.active_count()} > {self.batch_max_threads} => waiting 2 seconds')
                 time.sleep(2)
         else:
             future = self.publisher_client.publish(self.topic_path, data)
-            logging.info(f"Published message ID: {future.result()}")
+            logging.info(f'Published message ID: {future.result()}')
 
     def callback(self,future):
-        logging.debug(f"Published message ID: {future.result()}")
-
-
+        logging.debug(f'Published message ID: {future.result()}')
