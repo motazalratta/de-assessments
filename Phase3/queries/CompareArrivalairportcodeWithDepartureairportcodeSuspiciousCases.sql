fba76982365d6c7a659656656351ab93d7888900
Motaz Alratta <motaz.r@gmail.com>
2021-05-02 22:56:59 +0300

    fixed typo

--- Phase3/queries/CompareArrivalairportcodeWithDepartureairportcodeSuspiciousCases.sql
+++ Phase3/queries/CompareArrivalairportcodeWithDepartureairportcodeSuspiciousCases.sql
@@ -7,14 +7,15 @@ from
     select
         transactiondateutc,
         uniqueid,
-        itinerary lag(segment_arrivalairportcode,
-        1) over(partition by uniqueid
-    order by
-        segment_segmentnumber,
-        segment_legnumber) before_segment_arrivalairportcode,
+        itinerary,
+        lag(segment_arrivalairportcode,1) 
+            over(partition by uniqueid
+                order by segment_segmentnumber,
+                         segment_legnumber
+                ) before_segment_arrivalairportcode,
         segment_departureairportcode,
         segment_arrivalairportcode
     from
-        `assessmentdb.transactions` )
+        `assessmentdb.transactions`)
 where
     segment_departureairportcode <> before_segment_arrivalairportcode
